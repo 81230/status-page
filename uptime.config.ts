f813b3b1b86652a8b6faeefa33d1c4e38d2d4335
81230 <30727506+81230@users.noreply.github.com>
2025-03-02 20:40:38 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,55 +1,55 @@
 const pageConfig = {
-  title: "sun's Status Page"，
+  title: "lyc8503's Status Page",
   links: [
-    { link: 'https://github.com/lyc8503'， label: 'GitHub' }，
-    { link: 'mailto:me@lyc8503.site'， label: 'Email Me'， highlight: true }，
-  ]，
+    { link: 'https://github.com/lyc8503', label: 'GitHub' },
+    { link: 'mailto:me@lyc8503.site', label: 'Email Me', highlight: true },
+  ],
 }
 
 const workerConfig = {
-  kvWriteCooldownMinutes: 3，
-  passwordProtection: 'username:password'，
+  kvWriteCooldownMinutes: 3,
+  passwordProtection: 'username:password',
   monitors: [
     {
-      id: 'google_monitor'，
-      name: 'My Google Monitor'，
-      method: 'GET'，
+      id: 'google_monitor',
+      name: 'My Google Monitor',
+      method: 'GET',
       target: 'https://www.google.com'
-    }，
+    },
     {
-      id: 'ssh_monitor'，
-      name: 'Example SSH Monitor'，
-      method: 'TCP_PING'，
+      id: 'ssh_monitor',
+      name: 'Example SSH Monitor',
+      method: 'TCP_PING',
       target: 'router.wg.812300.xyz:22'
-    }，
-  ]，
+    },
+  ],
   callbacks: {
     onStatusChange: async (
-      env: any，
-      monitor: any，
-      isUp: boolean，
-      timeIncidentStart: number，
-      timeNow: number，
-      reason: string，
+      env: any,
+      monitor: any,
+      isUp: boolean,
+      timeIncidentStart: number,
+      timeNow: number,
+      reason: string,
     ) => {
       // This callback will be called when there's a status change for any monitor
       // Write any Typescript code here
 
       // This will not follow the grace period settings and will be called immediately when the status changes
       // You need to handle the grace period manually if you want to implement it
-    }，
+    },
     onIncident: async (
-      env: any，
-      monitor: any，
-      timeIncidentStart: number，
-      timeNow: number，
-      reason: string，
+      env: any,
+      monitor: any,
+      timeIncidentStart: number,
+      timeNow: number,
+      reason: string,
     ) => {
       // This callback will be called EVERY 1 MINTUE if there's an on-going incident for any monitor
       // Write any Typescript code here
-    }，
-  }，
+    },
+  },
 }
 
 // Don't forget this, otherwise compilation fails.
-export { pageConfig， workerConfig }
+export { pageConfig, workerConfig }
